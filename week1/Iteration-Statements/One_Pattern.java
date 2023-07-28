cf8455370657bf5fdc8dcafcd6639ed365b7b19e
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-07-28 12:21:31 +0530

    Update One_Pattern.java

--- week1/Iteration-Statements/One_Pattern.java
+++ week1/Iteration-Statements/One_Pattern.java
@@ -14,3 +14,35 @@ import java.util.*;
       
     }
   }
+
+
+// Question:
+// One Pattern
+// Print in the output the exact pattern provided below
+// 1
+// 1 1
+// 1 1 1
+// 1 1 1 1
+// 1 1 1 1 1
+
+// Input format
+// A single integer 
+// N
+
+// Output format
+// Print the same pattern provided in the problem statement.
+
+// Constraints
+// N
+// =
+// 1
+
+// Input
+// 1
+
+// Output
+// 1
+// 1 1
+// 1 1 1
+// 1 1 1 1
+// 1 1 1 1 1
